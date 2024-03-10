6f79784139f74c34a1b306b8dab10e1194fcac9b
Alfonso <alfoval26@gmail.com>
2024-03-10 12:44:55 -0300

    tarea de todo list con react y fetch

---
+++ src/App.jsx
@@ -0,0 +1,136 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [textInput, updateTextInput] = useState("");
+  let [todoList, updateTodoList] = useState([]);
+  let [userName, updateUsername] = useState("Alfonso")
+
+  useEffect(() => {
+    createUser(userName);
+    getUserTodoList();
+  }, []);
+
+  function handleInputOnChange(evento){
+    updateTextInput(evento.target.value);
+  }
+
+  function handleCreateTaskOnClick(evento){
+    createTodo();
+  }
+
+  function handleCreateTaskOnKeyDown(evento){
+
+    if(evento.key === "Enter"){
+      createTodo();
+    }
+  }
+
+  function handleUpdateTaskOnClick(index, done){
+    todoList[index].done = !done;
+    fetch("https://playground.4geeks.com/apis/fake/todos/user/" + userName,{
+      method: "PUT",
+      body: JSON.stringify(todoList),
+      headers:{
+        "Content-Type": "application/json",
+      }
+    })
+      .then((response)=>{
+        return response.json();
+      })
+      .then((data)=>{
+        console.log(data)
+        getUserTodoList();
+      })
+      .catch((error) =>{
+        console.log(error);
+      });
+
+  }
+
+  function createUser(name){
+    fetch("https://playground.4geeks.com/apis/fake/todos/user/" + name,{
+      method: "POST",
+      body:JSON.stringify([]),
+      headers:{
+        "Content-Type": "application/json",
+      }
+    })
+    .then((response)=>{
+      return response.json();
+    })
+    .then((data)=>{
+      console.log(data)
+    })
+    .catch((error) =>{
+      console.log(error);
+    });
+  }
+
+  function getUserTodoList(){
+    fetch("https://playground.4geeks.com/apis/fake/todos/user/"+ userName)
+      .then((response)=>{
+        return response.json()
+      })
+      .then((data)=>{
+        console.log(data);
+        updateTodoList(data);
+      })
+      .catch((error)=>{
+        console.log(error);
+      });
+  }
+
+  function createTodo(){
+    let nuevoArray = Array.from(todoList);
+    nuevoArray.push(
+      {
+        done: false,
+        label: textInput
+      }
+    );
+    fetch("https://playground.4geeks.com/apis/fake/todos/user/" + userName,{
+      method: "PUT",
+      body: JSON.stringify(nuevoArray),
+      headers:{
+        "Content-Type": "application/json",
+      }
+    })
+      .then((response)=>{
+        return response.json();
+      })
+      .then((data)=>{
+        console.log(data)
+        getUserTodoList();
+      })
+      .catch((error) =>{
+        console.log(error);
+      });
+  }
+
+
+  return (
+  <>
+    <h1>Todo list</h1>
+    <input type="text" onChange={handleInputOnChange} onKeyDown={handleCreateTaskOnKeyDown} />
+    <button onClick={handleCreateTaskOnClick}>Agregar Tarea</button>
+    <ul>
+      {
+        todoList.map((item, index)=>{
+          let style = {}
+
+          if (item.done === true) {
+            style = {textDecoration:"line-through"}
+          }
+          return <li style={style} key={index}>{item.label} <button onClick={()=> handleUpdateTaskOnClick(index, item.done)}>Marcar</button></li>
+        })
+      }
+    </ul>
+  </>
+  )
+}
+
+export default App
